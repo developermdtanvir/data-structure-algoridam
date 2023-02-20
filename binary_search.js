ac981c41c082414405fd330ddd5194d6b5d48622
devmdtanvir <officialtanvir@hotmail.com>
2023-02-20 09:39:05 +0600

    i am added binary_search.js file and ready to push code github

--- binary_search.js
+++ binary_search.js
@@ -1,32 +1,32 @@
-let exampleArray = [1, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59];
 
-const binarySearch = (array, target) => {
-    let startIndex = 0;
-    let endIndex = array.length - 1;
+function binarySearch(array, target) {
+    let start = 0;
 
-    while (startIndex <= endIndex) {
-        let middleIndex = Math.floor((startIndex + endIndex) / 2);
+    let end = array.length - 1;
 
-        if (target === array[middleIndex]) {
-            return console.log("Target was found at index " + middleIndex);
+    while (start <= end) {
+        let med = Math.floor((start + end) / 2);
+
+        if (array[med] === target) {
+            console.log(`Target Element by Binary_search ${array[med]}`)
+
+            return med
         }
 
-        if (target > array[middleIndex]) {
-            console.log("Searching the right side of Array")
-
-            startIndex = middleIndex + 1;
+        if (array[med] < target) {
+            start = med + 1;
         }
 
-        if (target < array[middleIndex]) {
-            console.log("Searching the left side of array")
-
-            endIndex = middleIndex - 1;
-        }
-
-        else {
-            console.log("Not Found this loop iteration. Looping another iteration.")
+        if (array[med] > target) {
+            end = med - 1;
         }
     }
 
-    console.log("Target value not found in array");
+    return -1;
+}
+
+const haveNumber = binarySearch([1, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59], 37)
+
+if (haveNumber === -1) {
+    console.log('Number not found this array')
 }
