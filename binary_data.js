1b377fa9f93e24bf6373335609fc893604d14a2a
devmdtanvir <officialtanvir@hotmail.com>
2023-02-20 08:34:01 +0600

    I am added all file

---
+++ binary_data.js
@@ -0,0 +1,9 @@
+/* 
+* Tree Data Strecture 
+* 1.Binary Data Structure 
+* 2. Binary searh tree 
+* ----------------------
+*   Bredth Frist Search 
+* 1. Depth-frist-search
+* 2. Bredth-frist-search
+*/
